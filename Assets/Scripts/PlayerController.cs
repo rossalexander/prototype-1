d93190632153ef132c1ea1443db0980a087964fb
Ross Alexander <rossaalexander@gmail.com>
2021-10-13 11:59:02 +0200

    Customize vehicle's speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
