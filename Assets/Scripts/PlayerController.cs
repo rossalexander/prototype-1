638cb9588c89c56b857b7a69ef61cee76553cb9a
Ross Alexander <rossaalexander@gmail.com>
2021-10-13 13:27:42 +0200

    Add adjustable speed variable for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
